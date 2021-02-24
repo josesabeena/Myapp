68b6dc5d6103306902ecedc5c930737be20d0bb6
boopathiraj <kmsb987@gmail.com>
2021-02-24 12:34:59 +0530

    bug fixed

---
+++ src/app/data/image.data.ts
@@ -0,0 +1,27 @@
+export const images = [
+    {
+      id: 1,
+      url: "../../assets/download1.jpg"
+    },
+    {
+      id: 2,
+      url: "../../assets/download2.jpg"
+    },
+    {
+      id: 3,
+      url: "../../assets/download3.jpg"
+    }, 
+
+    {
+      id: 4,
+      url: "../../assets/download2.jpg"
+    },
+    {
+      id: 5,
+      url: "../../assets/download3.jpg"
+    },
+    {
+      id: 6,
+      url: "../../assets/download1.jpg"
+    }, 
+  ];
